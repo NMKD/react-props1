31f0663afe059bfe159f431553b77b2392b4175e
kristina <kistina@localhost>
2021-11-11 09:07:45 +0300

    react-props-1 delete typeof in fn.Stars

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid'
 
 function Stars({ count }) {
-    if (typeof count !== 'number' || isNaN(count) || count < 1 || count > 5) {
+    if (count < 1 || count > 5) {
         return null;
     };
 
@@ -11,8 +11,6 @@ function Stars({ count }) {
     for (let i = 0; i < count; i += 1) {
         starsElements.push(<Star key={nanoid(10)} />);
     }
-
-    console.log(count);
 
     return (
         <ul className="card-body-stars u-clearfix">
